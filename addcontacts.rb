d1beb9c4189b3fb15840e7d5079c7f8d207439ef
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-23 14:53:46 -0500

    Added a receive_contact_info method and moved a file called contacts.rb out of this repository

---
+++ addcontacts.rb
@@ -0,0 +1,12 @@
+def receive_contact_info
+	puts "First Name"
+	first_name=gets.chomp
+	puts "Last Name"
+	last_name=gets.chomp
+	puts "Email"
+	email=gets.chomp
+	puts "Notes"
+	note=gets.chomp
+end
+
+receive_contact_info
